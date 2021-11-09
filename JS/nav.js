569694182f487d0fb0cd0c24e33c008c3e6aeae1
Jo <johana.chazarocortes@calbaptist.edu>
2021-11-09 09:28:11 -0800

    fixed path for navbar and css

--- JS/nav.js
+++ JS/nav.js
@@ -1,4 +1,4 @@
-fetch('html/nav.html')
+fetch('../html/nav.html')
     .then(res => res.text())
     .then(text => {
         let oldelem = document.querySelector("script#replace_with_navbar");
@@ -6,3 +6,20 @@ fetch('html/nav.html')
         newelem.innerHTML = text;
         oldelem.parentNode.replaceChild(newelem, oldelem);
     })
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
